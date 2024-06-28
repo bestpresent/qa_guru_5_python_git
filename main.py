dd14c8d56383147cfb44662003e2dd8a0f8c4fbc
Nataly Isaeva <mamadany25@gmail.com>
2024-06-28 09:14:08 +0400

    11

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Богдан)
+    print_hi('Богдан')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
